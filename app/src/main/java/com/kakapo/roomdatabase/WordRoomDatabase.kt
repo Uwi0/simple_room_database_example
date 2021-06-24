b3b926be5fa760e0707fadbaf3a2517fc6765837
Dwi Prasetyo <kakapoweb@gmail.com>
2021-06-24 09:51:50 +0700

    populate the database

--- app/src/main/java/com/kakapo/roomdatabase/WordRoomDatabase.kt
+++ app/src/main/java/com/kakapo/roomdatabase/WordRoomDatabase.kt
@@ -4,32 +4,62 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 // Annotates class to be a Room Database with a table (entity) of the word class
 @Database(entities = [Word::class], version = 1, exportSchema = false)
-abstract class WordRoomDatabase : RoomDatabase(){
+abstract class WordRoomDatabase : RoomDatabase() {
 
-    abstract fun wordDao() : WordDao
+    abstract fun wordDao(): WordDao
 
-    companion object{
+    companion object {
         //singleton prevent multiple instance of database opening at
         //the same time
 
         @Volatile
         private var INSTANCE: WordRoomDatabase? = null
 
-        fun getDatabase(context: Context): WordRoomDatabase{
+        fun getDatabase(context: Context, scope: CoroutineScope): WordRoomDatabase {
             // if the INSTANCE is not null, then return it
             // if it is, then create the database
-            return INSTANCE ?: synchronized(this){
+            return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     WordRoomDatabase::class.java,
                     "word_database"
-                ).build()
+                )
+                    .addCallback(WordDatabaseCallback(scope))
+                    .build()
 
                 INSTANCE = instance
                 instance
+            }
+        }
+
+        private class WordDatabaseCallback(
+            private val scope: CoroutineScope
+        ) : RoomDatabase.Callback() {
+
+            override fun onCreate(db: SupportSQLiteDatabase) {
+                super.onCreate(db)
+                INSTANCE?.let { database ->
+                    scope.launch {
+                        populateDatabase(database.wordDao())
+                    }
+                }
+            }
+
+            suspend fun populateDatabase(wordDao: WordDao) {
+                //Delete all content here
+                wordDao.deleteAll()
+
+                //add some sample words
+                var word = Word("Hello")
+                wordDao.insert(word)
+                word = Word("zawarudo")
+                wordDao.insert(word)
             }
         }
 
